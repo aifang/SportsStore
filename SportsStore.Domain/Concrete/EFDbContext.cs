495f1fb14096cecdce8fc18424e70197ba62bb75
aifang <aifanghang@163.com>
2015-07-29 01:20:51 +0800

    EF5连接oracle报错表或视图不存在

--- SportsStore.Domain/Concrete/EFDbContext.cs
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -10,7 +10,7 @@ namespace SportsStore.Domain.Concrete
     /// <summary>
     /// 类似数据库里面的sql窗口，可以执行sql，作为事务中的内容，确定后可以提交
     /// </summary>
-    public class EFDbContext : System.Data.Entity.DbContext
+    public class EFDbContext : DbContext
     {
         /// <summary>
         /// 属性名 Products（库中的表名），用Product实体类来表示
