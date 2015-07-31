47ce07b1c4a088d7aebd3095ef4e5b2a362505aa
aifang <aifanghang@163.com>
2015-08-01 00:49:09 +0800

    localdb

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -12,6 +12,6 @@ namespace SportsStore.Domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Categroy { get; set; }
+        public string Category { get; set; }
     }
 }
