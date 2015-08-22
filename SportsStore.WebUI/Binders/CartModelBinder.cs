3e61704d63dce943ebc6246c46f24266d557e7a2
aifang <aifanghang@163.com>
2015-08-22 12:54:30 +0800

    cart

---
+++ SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Binders
+{
+    public class CartModelBinder:IModelBinder
+    {
+        private const string sessionKey="Cart";
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            //通过会话获取Cart
+            Cart cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            //若会话没有Cart，则创建一个
+            if(cart==null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            //Cart
+            return cart;
+        }
+    }
+}
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Binders;
 
 namespace SportsStore.WebUI
 {
@@ -24,6 +26,7 @@ namespace SportsStore.WebUI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
